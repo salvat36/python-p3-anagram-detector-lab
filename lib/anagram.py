1117a1df4fdc0d1597fb4d77a36a58ccaea6e819
Ryan <salvat36@gmail.com>
2023-05-23 17:10:57 -0500

    passing lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        return [el for el in words if sorted(self.word) == sorted(el)]
